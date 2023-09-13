b303455f108702af275fff078f4073b8de53b86a
prottasha86nag <136179738+prottasha86nag@users.noreply.github.com>
2023-09-13 20:02:20 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,56 @@
+
+
+//text-change
+
+function textChange(){
+   document.getElementById( 'h2' ).innerHTML="Hello World"
+
+}
+
+
+//t3_change
+
+function t3Change(){
+    document.getElementById( 't3' ).innerHTML="How Was Your Day?"
+}
+
+//t4_change
+
+function t4Change(){
+    document.getElementById( 't4' ).innerHTML="Hope You Had A Nice Weekend"
+}
+
+//t6_change
+
+function t6Change(){
+    document.getElementById( 't6' ).innerHTML="Very Well"
+}
+
+//t7_change
+
+function t7Change(){
+    document.getElementById( 't7' ).innerHTML="Great"
+}
+
+
+//t9_change
+
+function t9Change(){
+    document.getElementById( 't9' ).innerHTML="Bye"
+}
+
+//img_chnage
+
+function onChange(){
+    document.getElementById( 'img' ).src="images/on1.jpg"
+}
+
+function offChange(){
+    document.getElementById( 'img' ).src="images/off1.jpg"
+}
+
+//p_change
+
+function mouseOver(){
+    document.getElementById( 'p_chang' ).style.color="green"
+}
